2bcdc53faed19d3a337b1e29740358704b77e2ee
farheensamad <fsamad28@gmail.com>
2024-12-25 18:43:36 -0500

    Create the RestController HTTP POST endpoint

--- explorecali-jpa/src/main/java/com/example/explorecalijpa/web/TourRatingController.java
+++ explorecali-jpa/src/main/java/com/example/explorecalijpa/web/TourRatingController.java
@@ -1,15 +1,12 @@
 package com.example.explorecalijpa.web;
 
+import java.util.List;
+import java.util.Map;
 import java.util.NoSuchElementException;
 
+import com.example.explorecalijpa.model.TourRating;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.example.explorecalijpa.business.TourRatingService;
 
@@ -38,9 +35,32 @@ public class TourRatingController {
   @PostMapping
   @ResponseStatus(HttpStatus.CREATED)
   public void createTourRating(@PathVariable(value = "tourId") int tourId,
-      @RequestBody @Valid RatingDto ratingDto) {
+                               @RequestBody @Valid RatingDto ratingDto) {
     tourRatingService.createNew(tourId, ratingDto.getCustomerId(), ratingDto.getScore(), ratingDto.getComment());
   }
+
+  @GetMapping
+  public List<RatingDto> getAllRatingsForTour(@PathVariable(value = "tourId") int tourId) {
+    List<TourRating> tourRatings = tourRatingService.lookupRatings(tourId);
+    return tourRatings.stream().map(RatingDto::new).toList();
+  }
+
+  @GetMapping("/average")
+  public Map<String, Double> getAverage(@PathVariable(value = "tourId") int tourId) {
+    return Map.of("average", tourRatingService.getAverageScore(tourId));
+  }
+
+  @PutMapping
+  public RatingDto updateWithPut(@PathVariable(value = "tourId") int tourId,
+                                 @RequestBody @Valid RatingDto ratingDto) {
+    return new RatingDto((tourRatingService.update(tourId, ratingDto.getCustomerId(), ratingDto.getScore(), ratingDto.getComment())));
+  }
+
+  @DeleteMapping("{customerId}")
+  public void delete(@PathVariable(value ="tourId") int tourID, @PathVariable(value ="customerId") int customerId)
+  {
+    tourRatingService.delete(tourID,null);
+}
 
   @ExceptionHandler(NoSuchElementException.class)
   @ResponseStatus(HttpStatus.NOT_FOUND)
